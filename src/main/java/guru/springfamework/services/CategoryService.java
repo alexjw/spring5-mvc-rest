cad677a12380413c9107c20b4966cc14b616cde5
Alex J <abel.oalex@gmail.com>
2019-12-17 10:31:32 -0300

    Added categoryController

--- src/main/java/guru/springfamework/services/CategoryService.java
+++ src/main/java/guru/springfamework/services/CategoryService.java
@@ -2,9 +2,11 @@ package guru.springfamework.services;
 
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CategoryService extends BaseService<Category, CategoryDTO> {
 
     CategoryDTO getByName(String name);
